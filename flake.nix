37f8e94d7d386e529659646dd2b504ccc9e5a2ff
Deadbeef <ent3rm4n@gmail.com>
2026-09-15 20:46:11 -0400

    add .envrc and flake for dev shell

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs = {
+    nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.zst";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in {
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          uv
+          cmake
+          ccache
+          clang_21
+          ninja
+          python3
+        ];
+      };
+    };
+}
